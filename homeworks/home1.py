8e8be97ba45a927521af9c19966c48b4da1b3683
gazizova68 <gazizova6886@gmail.com>
2023-12-07 12:20:43 -0500

    home1

--- homeworks/home1.py
+++ homeworks/home1.py
@@ -1,13 +1,12 @@
 class Home:
     def __new__(cls, *args, **kwargs):
         return Home
+    n = input("Введите число: ")
+    try:
+        n = int(n)
+        print("Верный ввод")
+    except ValueError:
+        print("Вводите только целые числа!")
 
 
 home = Home
-
-n = input("Введите число: ")
-try:
-    n = int(n)
-    print("Верный ввод")
-except ValueError:
-    print("Вводите только целые числа!")
