581ebf835e1d9db748fde0e8195b3427f93e29a1
gazizova68 <gazizova6886@gmail.com>
2024-01-02 14:05:30 -0500

    aster

--- homeworks/hero.py
+++ homeworks/hero.py
@@ -31,18 +31,76 @@ print(hero.__str__())
 
 
 class EarthHero(SuperHero):
-    def damage(self):
-        print(False)
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = False
 
-    def fly(self):
-        print(False)
+    def double_health_points(self):
+        return int(self.health_points) ** 2
+        self.fly = True
 
-    def run(self):
-        print('Здоровье героя возведено в квадрат:', int(self.health_points)**2)
+    def true_in_true_phrase(self, true_phrase):
+        return true_phrase in str(self.fly)
 
 
-thanos = EarthHero('Thanos', 'Titan', 'immortality', '168909', 'will destroy this universe', )
-# print(thanos.__mul__(2))
-# print(thanos.__len__())
-# print(thanos.__str__())
-thanos.run()
+class AirHero(SuperHero):
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = False
+
+    def double_health_points(self):
+        return int(self.health_points) ** 2
+        self.fly = True
+
+    def true_in_true_phrase(self, true_phrase):
+        return true_phrase in str(self.fly)
+
+
+class CosmicHero(SuperHero):
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = False
+
+    def double_health_points(self):
+        return int(self.health_points) ** 2
+        self.fly = True
+
+    def true_in_true_phrase(self, true_phrase):
+        return true_phrase in str(self.fly)
+
+
+class Villain(CosmicHero):
+     people = 'monster'
+
+     def gen_x(self):
+         pass
+
+     def crit(self, other_hero):
+         other_hero.health_points **= self.damage
+
+
+earth_hero = EarthHero('Thanos', 'Titan', 'immortality', '168909', 'will destroy this universe', '80089')
+air_hero = AirHero('Sky Hero', 'Твой герой', 'непобедимый', '1029384', 'Воздушный защитник', '76567')
+cosmic_hero = CosmicHero('Super Star', 'звездун', 'самый быстрый', '298709', 'Галактические герой', '5678')
+villain = Villain('Dark hero', 'Владыка Тьмы', 'РАзрушение всего живого', '142536475', 'Ваш конец близок', '456787')
+
+print(f"Никнейм:", earth_hero.nickname)
+print(f"Здоровье возведено в квдрат:", earth_hero.double_health_points())
+print(f"Ваш урон:", earth_hero.damage)
+
+print(f"Никнейм:", air_hero.nickname)
+print(f"Здоровье возведено в квдрат:", air_hero.double_health_points())
+print(f"Ваш урон:", air_hero.damage)
+print(air_hero.true_in_true_phrase(True))
+
+print(f"Никнейм:", cosmic_hero.nickname)
+print(f"Здоровье возведено в квдрат:", cosmic_hero.double_health_points())
+print(f"Ваш урон:", cosmic_hero.damage)
+
+villain.crit(air_hero)
+print(f"Здоровье {air_hero.name} после атаки врага: {air_hero.health_points}")
